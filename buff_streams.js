dddaf2c21187e2473347e5c386799cb4dc5d4ed4
javascript-developer <javascript-developer@gmail.com>
2016-08-29 16:23:30 +0530

    SD

--- buff_streams.js
+++ buff_streams.js
@@ -17,3 +17,19 @@ var readableStream = fs.createReadStream('file.exe');
 var writableStream = fs.createWriteStream('file2.exe');
 
 readableStream.pipe(writableStream);
+
+
+/*
+
+
+https://www.sitepoint.com/basics-node-js-streams/
+
+https://mattdesl.svbtle.com/debugging-nodejs-in-chrome-devtools\
+
+https://remoteok.io/remote-jobs/18564-remote-node-js-developer-x-team-international-pty
+
+http://stackoverflow.com/jobs/119567/nodejs-developer-m-f-locafox-gmbh?offset=8&tags=nodejs&sort=s
+
+http://stackoverflow.com/jobs/78892/senior-javascript-engineer-berlin-mycs-gmbh?offset=12&tags=nodejs&sort=s
+
+*/
